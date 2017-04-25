376cd35383b94c930d52cfa09ac26f10d9d7c76c
gxx <562606139@qq.com>
2017-04-25 22:18:07 +0800

    4.25

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,8 @@ router.get('/', function (ctx, next) {
   ctx.body = 'this a users response!';
 });
 
+router.get('/hello', function (ctx, next) {
+  ctx.body = 'hello ';
+});
+
 module.exports = router;
